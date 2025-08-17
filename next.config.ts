faacf225e91bb4539f53bb30d5e93a89fb687f70
yukenz <yuyun.purniawan@gmail.com>
2025-08-17 10:26:43 +0700

    Fix: Allow Dev Origins

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    /* config options here */
+    reactStrictMode: true,
+    allowedDevOrigins: (process.env.CORS_DOMAIN as string).split('|'),
 };
 
 export default nextConfig;
