cda0f89dc16b32fb884294f0d0404ddc8b721b7b
yukenz <yuyun.purniawan@gmail.com>
2025-08-23 22:21:52 +0700

    Fix: Mock Midtrans Create Trx

--- src/pages/api/mock/midtrans/create-transactions.ts
+++ src/pages/api/mock/midtrans/create-transactions.ts
@@ -8,7 +8,7 @@ async function postProcessor(
 ) {
 
     res.setHeader('Content-Type', 'application/json')
-    res.status(200).json({
+    res.status(201).json({
         "token": "66e4fa55-fdac-4ef9-91b5-733b97d1b862",
         "redirect_url": "https://app.sandbox.midtrans.com/snap/v2/vtweb/66e4fa55-fdac-4ef9-91b5-733b97d1b862"
     });
