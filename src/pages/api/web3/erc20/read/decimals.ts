8cf35ec2b6dbcab9371f95c4992c83ae5d24864b
yukenz <yuyun.purniawan@gmail.com>
2025-08-24 12:43:44 +0700

    Feat: ERC20 Decimals

---
+++ src/pages/api/web3/erc20/read/decimals.ts
@@ -0,0 +1,54 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+import {getPublicClient, KeyRegisteredChain, registeredChain} from "@/lib/viem";
+import * as z from "zod";
+import {handleError, handleMethodNotAllowed} from "@/lib/error";
+import {erc20Abi} from "@/abi/erc20";
+import {jsonToString} from "@/lib/utils";
+import {hexString} from "@/lib/zod";
+
+
+const ERC20DecimalsRequest = z.object({
+    chain: z.enum(Object.keys(registeredChain) as [KeyRegisteredChain]),
+    erc20Address: hexString()
+});
+
+
+async function postProcessor(
+    req: NextApiRequest,
+    res: NextApiResponse<{}>,
+) {
+
+    const {
+        erc20Address,
+        chain
+    } = ERC20DecimalsRequest.parse(req.body);
+
+    const publicClient = getPublicClient({chain});
+
+    const data = await publicClient.readContract({
+        address: erc20Address as `0x${string}`,
+        abi: erc20Abi,
+        functionName: 'decimals'
+    })
+
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(jsonToString({data}));
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<{}>,
+) {
+    const {query, method} = req;
+    try {
+        switch (method) {
+            case "POST":
+                await postProcessor(req, res);
+                break;
+            default:
+                handleMethodNotAllowed(method, ["POST"], res)
+        }
+    } catch (err) {
+        handleError(err, res)
+    }
+}
